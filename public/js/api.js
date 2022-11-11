a5c4caeb0c509f8f87c672560687e84aa198a7eb
Abhishek Thomas <111083527+abhhishekk@users.noreply.github.com>
2022-11-12 00:38:33 +0530

    First Commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "6c068c06200b680ff1f346bdc37b677e";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
